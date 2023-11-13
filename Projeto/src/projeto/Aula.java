421321b3c21f6c82c6f83f9f4bdf12016ac44279
CAIO DE SOUZA DE MEDEIROS <202203369008@alunos.ibmec.edu.br>
2023-11-13 09:12:47 -0300

    13/11/2023 09:12

---
+++ Projeto/src/projeto/Aula.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 202203369008
+ */
+public class Aula {
+    
+    String data;
+    String modalidade;
+    String instrutor;
+    int id;
+    String cliente;
+    
+    public Aula() {
+        
+    }
+}
